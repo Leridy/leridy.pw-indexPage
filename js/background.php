484c6087543ba8382293866b353a1a94e64eecf9
leridy <leiyudibb@gmail.com>
2015-09-13 22:34:20 +0800

    首次创建
    首次创建

--- js/background.php
+++ js/background.php
@@ -1,6 +1,6 @@
-<?php 
-    $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&#038;n=1');
-    if(preg_match("/<url-->(.+?)&lt;\/url&gt;/ies",$str,$matches)){
+<?php
+    $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
+    if(preg_match("/<url>(.+?)<\/url>/ies",$str,$matches)){
         $imgurl='http://cn.bing.com'.$matches[1];
     }
     if($imgurl){
